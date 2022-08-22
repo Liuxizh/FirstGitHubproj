f495bfa9fcad0ebf943a858149f2b36cf27fdbf6
Liuxizh <lxz_gz_2022@163.com>
2022-08-22 15:56:06 +0800

    测试2

--- Helloword.java
+++ Helloword.java
@@ -2,6 +2,6 @@ package java_basics.part_one;
 
 public class Helloword {
     public static void main(String[] args){
-        System.out.println("hello word");
+        System.out.println("hello word123");
     }
 }
